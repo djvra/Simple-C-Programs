1babad52f14de58bef4ace4a1319d3898000a07b
djvra <76538049+djvra@users.noreply.github.com>
2021-02-06 14:29:33 +0300

    Update Pascal's Triangle.c

--- Pascal's Triangle.c
+++ Pascal's Triangle.c
@@ -8,11 +8,11 @@ int main(){
     int b=limit;
     
     for(int i=0; i<limit; ++i){
-    	for(space=b; space>0; --space) printf(" ");
+    	for(space=b; space>0; --space) printf("  ");
     	for(int j=0; j<=i; ++j){
     		if(j==0 || j==i) array[i][j]=1; 
     		else array[i][j]=array[i-1][j-1] + array[i-1][j];
-    		printf("%d ", array[i][j]);
+    		printf(" %2d ", array[i][j]);
 		}
 		printf("\n");
 		b-=1;
