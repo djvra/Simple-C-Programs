6b97bb6bb241806076831a0d2f7ee044d3a4b20a
djvra <76538049+djvra@users.noreply.github.com>
2021-02-06 11:20:43 +0300

    Add files via upload

---
+++ Pascal's Triangle.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+
+int main(){
+    int array[20][20];
+    int limit, space;
+    printf("how many rows do you want to print? ");
+    scanf("%d", &limit);
+    int b=limit;
+    
+    for(int i=0; i<limit; ++i){
+    	for(space=b; space>0; --space) printf(" ");
+    	for(int j=0; j<=i; ++j){
+    		if(j==0 || j==i) array[i][j]=1; 
+    		else array[i][j]=array[i-1][j-1] + array[i-1][j];
+    		printf("%d ", array[i][j]);
+		}
+		printf("\n");
+		b-=1;
+	}
+    return 0;
+}
+
